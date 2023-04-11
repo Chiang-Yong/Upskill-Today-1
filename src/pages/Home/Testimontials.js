c36909b17b01f4e12588f0f3bd46f2654caddbec
QianlingC <chowqianlingg@gmail.com>
2023-04-11 14:40:03 +0800

    testimonials

--- src/pages/Home/Testimontials.js
+++ src/pages/Home/Testimontials.js
@@ -1,9 +1,23 @@
 import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import TestimonialCarousel from "./TestimonialCarousel";
 const Testimontials = () => {
-    return (
-        <h1> Testimontials</h1>
-    )
+  return (
+    <Container fluid>
+      <Row className="px-5 py-5">
+        <Col md={6}>
+          <h1 className="fw-bold" style={{ color: "#ff7b00" }}>
+            Get Inspired
+          </h1>
+          <h3>View the success stories of our graduates</h3>
+        </Col>
 
-}
+        <Col md={6}>
+          <TestimonialCarousel />
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
 export default Testimontials;
