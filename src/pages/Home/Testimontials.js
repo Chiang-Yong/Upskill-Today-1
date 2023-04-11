c526346b2efb88359a7973abe76d81d493436857
blissfulwolf <cashgrid@yahoo.com>
2023-04-11 15:44:21 +0800

    Testimontials height

--- src/pages/Home/Testimontials.js
+++ src/pages/Home/Testimontials.js
@@ -11,10 +11,10 @@ const Testimontials = () => {
         <div className="testimontials">
             <Container fluid>
                 <Row className="px-5 py-3 mx-auto justify-content-around ">
-                    <Card style={{ width: "20rem" }}>
+                    <Card style={{ height: "40rem", width: "20rem" }}>
                         <Card.Body>
                             <Card.Title className="display-6 text-left">Get Inspired</Card.Title>
-                            <Card.Text className="fs-5 text-ledt">
+                            <Card.Text className="fs-5 text-left">
                                 View the success stories of our graduates
                             </Card.Text>
                         </Card.Body>
@@ -22,13 +22,7 @@ const Testimontials = () => {
 
                     <Card style={{ width: "60rem" }}>
 
-                        <Card.Body>
-                            <Card.Title className="text-left">CORPORATE</Card.Title>
-                            <Card.Text className="fs-5 text-left">
-                                Work With Us
-                            </Card.Text>
 
-                        </Card.Body>
                     </Card>
 
 
