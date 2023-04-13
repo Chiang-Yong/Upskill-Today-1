0957f34aa19af73b4befe25b1d762a565bd8c88e
Chiang-Yong <chiang_yong@yahoo.com>
2023-04-13 17:40:32 +0800

    Update the about pages.

--- src/pages/About/AboutUs.js
+++ src/pages/About/AboutUs.js
@@ -1,27 +1,38 @@
 import React from "react";
-import { Card } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import aboutUs from "../../images/AboutUs.jpg";
-
 import "./aboutUs.css";
 
 const AboutUs = () => {
   return (
-    <div className="about-section">
-      <h1 className="about-title">
-        <span data-text-fill='true' 
-        style={{backgroundImage:"linear-gradient(270deg, rgb(255, 123, 0) 0%, rgb(255, 208, 0) 100%)"}}
-        className="colorful-text">
-        Upskill Today 
-        </span>
-      </h1>
-      <p className="about-description">
-        An innovative and highly curated ​Hire, Upskill & Deploy Program.
-        We believe everyone should be given a chance to enhance the Digital Information 
-        skill through Upskill Today program.
-      </p>
-
-      <img src={aboutUs}></img>
-    </div>
+    <Container className="about-section" fluid='ture'>
+      <Row>
+        <Col>
+          <h1 className="about-title">
+            <span
+              data-text-fill="true"
+              style={{
+                backgroundImage:
+                  "linear-gradient(270deg, rgb(255, 123, 0) 0%, rgb(255, 208, 0) 100%)",
+              }}
+              className="colorful-text"
+            >
+              Upskill Today
+            </span>
+          </h1>
+          <p className="about-description">
+            An innovative and highly curated ​Hire, Upskill & Deploy Program.
+          </p>
+        </Col>
+      </Row>
+      <Row>
+        <img
+          src={aboutUs}
+          alt="Geco Company"
+          className="about-section-image"
+        ></img>
+      </Row>
+    </Container>
   );
 };
 
--- src/pages/About/WhatIsUpSkill.js
+++ src/pages/About/WhatIsUpSkill.js
@@ -1,23 +1,30 @@
 import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 import WhatIsUpskillToday from "../../images/WhatIsUpskillToday.png"
 import "./whatIsUpskill.css";
 
 
 const WhatIsUpSkilled = () => {
   return (
-    <div className="what-upskill d-inline-block">
-      <img src={WhatIsUpskillToday} alt="what is upskill" />
-      <div className="centered">
-        <h1 className="text-center" style={{color:'#ff7b00'}}>What is Upskill Today?</h1>
-        <p className="text-left">
+    <Container className="what-upskill" fluid='ture'>
+      <Row>
+        <Col>
+         <img src={WhatIsUpskillToday} 
+         alt="what is upskill"
+         />
+        </Col>
+        <Col style={{alignItems:'center'}}>
+          <h1 style={{marginTop:"10%"}}>What is Upskill Today?</h1>
+          <p>
           Upskill Today is a highly curated talent acquisition and talent
           retention strategy. Under this model, we select top candidates from
           our extended pool of recent graduates, technical professionals
           with obsolete or soon-to-be deprecated technology skills, and
           mid-career professionals seeking to move to Next-Gen technology roles.
-        </p>
-      </div>
-    </div>
+          </p>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
