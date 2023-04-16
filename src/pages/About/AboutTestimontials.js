bab5c7255a25da353f911d9bad42d9b1261285b0
Chiang-Yong <chiang_yong@yahoo.com>
2023-04-16 20:22:13 +0800

    update the about testimonial css

--- src/pages/About/AboutTestimontials.js
+++ src/pages/About/AboutTestimontials.js
@@ -9,14 +9,14 @@ const Testimontials = () => {
         <div className="about-testimontial">
             <Container fluid>
                 <Row className="px-5 py-3 mx-auto justify-content-around ">
-                    <Card style={{ width: "40rem" }}>
+                    <Card className= "border-2" style={{ width: "40rem" }}>
 
                         <Card.Body>
                             <h1>Join Upskill Today!</h1>
-                            <Card.Text className="fs-5 text-left">
+                            <Card.Text className="text-left">
                                 If you are looking to enhance your skills, you have coming at the right place. We are looking for candidates who are open to learn new skills to improve your career prospects and keep up with changing technologies.
                             </Card.Text>
-                            <Button variant="warning">Join Upskill Today</Button>
+                            <Button style={{backgroundColor:'#ff7b00'}}>Join Upskill Today</Button>
                         </Card.Body>
                     </Card>
 
@@ -25,10 +25,10 @@ const Testimontials = () => {
                         <Card.Body>
                             <h1>Engage Upskill Today!</h1>
 
-                            <Card.Text className="fs-5 text-left">
+                            <Card.Text className="text-left">
                                 We provide companies with a pool of top talents that have trained in the latest skills and technologies. We also offer customized solutions that can be tailored to the specific needs of your business.
                             </Card.Text>
-                            <Button variant="warning">Explore More</Button>
+                            <Button style={{backgroundColor:'#ff7b00'}}>Explore More</Button>
                         </Card.Body>
                     </Card>
 
