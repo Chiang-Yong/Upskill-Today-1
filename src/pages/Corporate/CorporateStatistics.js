ce708cddb7696e679bd32f4975c3497c2df2552e
blissfulwolf <cashgrid@yahoo.com>
2023-04-17 13:18:21 +0800

    corporate statistics

--- src/pages/Corporate/CorporateStatistics.js
+++ src/pages/Corporate/CorporateStatistics.js
@@ -1,10 +1,45 @@
 import React from 'react'
+import { Row, Container, Card } from "react-bootstrap";
+
 import "./corporateStatistics.css"
 
 const CorporateStatistics = () => {
     return (
-        <div>CorporateStatistics</div>
-    )
-}
+        <div className="corporatestatistics">
+            <Container fluid>
+                <Row className="px-5 py-3 mx-auto justify-content-around ">
+                    <Card style={{ width: "18rem" }}>
+                        <Card.Body>
+                            <Card.Title className="display-4 text-center">500+</Card.Title>
+                            <Card.Text className="fs-5 text-center">
+                                Projects
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+
+                    <Card style={{ width: "18rem" }}>
+                        <Card.Body>
+                            <Card.Title className="display-4 text-center">45+</Card.Title>
+                            <Card.Text className="fs-5 text-center">
+                                Customers
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+
+                    <Card style={{ width: "18rem" }}>
+                        <Card.Body>
+                            <Card.Title className="display-4 text-center">100+</Card.Title>
+                            <Card.Text className="fs-5 text-center">
+                                Professionals
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Row>
+            </Container>
+        </div >
+
+    );
+
+};
 
 export default CorporateStatistics
