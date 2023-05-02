e0edac466baf8e7e3f16ab1b653660047481836e
blissfulwolf <cashgrid@yahoo.com>
2023-05-02 09:44:24 +0800

    Corporate Alignment

--- src/pages/Corporate/PartnerWithUs.js
+++ src/pages/Corporate/PartnerWithUs.js
@@ -7,16 +7,16 @@ import "./partnerWithUs.css"
 const PartnerWithUs = () => {
     return (
         <Container className="partnerus" fluid='true'>
-            <Row>
+            <Row xs={1} md={2} className="mx-auto" >
                 <Col>
-                    <h1 style={{ marginTop: "15%" }}>Partner With Us</h1>
+                    <h1 style={{ marginTop: "20%" }}>Partner With Us</h1>
                     <p>
                         {" "}
                         Become part of our global hiring network and find the talent your company or startup needs today.
                     </p>
 
                 </Col>
-                <Col style={{ alignContent: 'center', alignItems: "center" }}>
+                <Col style={{ marginTop: "5%", alignContent: 'center', alignItems: "center" }}>
                     <img src={corporate_partner} alt="corporate_partner" />
                 </Col>
             </Row>
